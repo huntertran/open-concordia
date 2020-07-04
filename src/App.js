d933c341e8b80ad380971259640f68ee9638dffa
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-07-03 21:41:57 -0400

    login success

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
@@ -11,8 +11,16 @@ import Admin from './pages/Admin';
 import { AuthContext } from "./context/auth";
 
 function App() {
+  const existingTokens = JSON.parse(localStorage.getItem("tokens"));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
   return (
-    <AuthContext.Provider value={false}>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
       <Router>
         <div>
           <ul>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,68 @@
-import React from "react";
-// import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from 'axios';
 import logoImg from './../logo.svg'
-import { Card, Logo, Form, Input, Button } from './../components/AuthForm'
+import { Card, Logo, Form, Input, Button, Error } from './../components/AuthForm'
+import { useAuth } from "../context/auth";
 
 function Login() {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const { setAuthTokens } = useAuth();
+
+    function postLogin() {
+        axios.get("https://opendata.concordia.ca/API/v1/course/catalog/filter/BIOL/200/UGRD", {
+            auth: {
+                username: username,
+                password: password
+            }
+        }).then(result => {
+            if (result.status === 200) {
+                setAuthTokens({
+                    username: username,
+                    password: password
+                });
+                setLoggedIn(true);
+            } else {
+                setIsError(true);
+            }
+        }).catch(e => {
+            setIsError(true);
+        });
+    }
+
+    if (isLoggedIn) {
+        return <Redirect to="/" />;
+    }
+
     return (
         <Card>
             <Logo src={logoImg} />
             <Form>
-                <Input type="username" placeholder="username" />
-                <Input type="key" placeholder="key" />
-                <Button>Sign In</Button>
+                <Input
+                    type="username"
+                    value={username}
+                    onChange={e => {
+                        setUsername(e.target.value);
+                    }}
+                    placeholder="email"
+                />
+                <Input
+                    type="password"
+                    value={password}
+                    onChange={e => {
+                        setPassword(e.target.value);
+                    }}
+                    placeholder="password"
+                />
+                <Button onClick={postLogin}>Sign In</Button>
             </Form>
             <a href="https://opendata.concordia.ca/admin/register.php"
                 rel="noopener noreferrer"
                 target="_blank">Don't have an account?</a>
+            {isError && <Error>The username or password provided were incorrect!</Error>}
         </Card>
     );
 }
